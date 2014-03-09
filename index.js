17c47400ddc4c450323c037cbb35d91e444a8d84
iamdoron <doronpagot@gmail.com>
2014-03-09 18:02:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-
+var Hoek = require('hoek');
 var nano = require('nano');
 var server = nano("http://isaacs.iriscouch.com/");
 var npm = server.db.use("registry");
 
 var findDependencies = function(npm, accumulatedPackges, packageName, onNewPackage) {
 	npm.get(packageName, function(err, npmPackage){
+		if (err) {
+			return onNewPackage(err, packageName);
+		}
 		for (version in npmPackage.versions) {
-			for (dependentPackage in npmPackage.versions[version].dependencies) {
+			var currentVersion = npmPackage.versions[version];
+			dependencies = [];
+			if (currentVersion.dependencies)
+				dependencies = Object.keys(currentVersion.dependencies)
+			if (currentVersion.peerDependencies)
+				dependencies = dependencies.concat(Object.keys(currentVersion.peerDependencies));
+			dependencies.forEach(function(dependentPackage){
 				if (!accumulatedPackges[dependentPackage])
 				{
-					onNewPackage(dependentPackage);
+					onNewPackage(undefined, dependentPackage);
 					accumulatedPackges[dependentPackage] = true;
 					process.nextTick(function(){
 						findDependencies(npm, accumulatedPackges, dependentPackage, onNewPackage);
 					});
 				}
-			}
+			});
 		}
 		module.exports
 	});
@@ -25,7 +34,7 @@ module.exports = function(initialPackages, onNewPackage){
 	packages = Object.create(null);
 	initialPackages.forEach(function(packageName){
 		packages[packageName] = true;
-		onNewPackage(packageName);
+		onNewPackage(undefined, packageName);
 		findDependencies(npm, packages, packageName, onNewPackage);
 	});
 };
